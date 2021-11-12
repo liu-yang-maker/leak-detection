a8616761011c8bc7889bceb38d92d06b73018b79
LiuY <1668210218@qq.com>
2021-11-12 16:25:03 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leak-detection
 原油泄漏监测系统
+
+> 附件是泄漏监测系统的数据，ldBin.py是读取文件的python程序，调用函数“readBin(filePath)”就可以。records.xlsx是测试记录。
+
---
+++ ldBin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import struct
+
+#读取泄漏监测系统bin文件
+def readBin(filePath):
+    #file_path = unicode(file_path,'utf8')
+    #file_path = str(file_path)
+    file = open(filePath,'rb')
+    
+    file.seek(120,0)
+    dataStr = file.read()
+    file.close()
+    
+    dataLen = int(len(dataStr)/4)
+    
+    data = struct.unpack('>'+str(dataLen)+'f',dataStr)
+    #data = struct.unpack('>71996f',data_str)
+
+    data = np.array(data)
+    #转换成列向量
+    data = data.reshape(data.size)
+    return data 
+
+#生成读取文件路径，LabView程序生成的bin文件
+def createBinPath(path,station_name,signal_type,station_type,year,month,day,hour):
+    #file_path = '%s/%s%s/%s/%02年/%02d月/%02d日/%s%s%02d时.bin' % (path,station_name,signal_type,station_type,year,month,day,station_name,station_type,hour)   
+    filePath = '%s/%s%s/%s/%d年/%02d月/%02d日/%s%s%02d时.bin' % (path,station_name,signal_type,station_type,year,month,day,station_name,station_type,hour) 
+    return filePath
